bc2e28d77d6fcc3346054b128754568b0eb40c7f
Sanjay Namdeo <sanjay.namdeo@live.com>
2020-05-30 10:41:49 +0200

    Use lodash to find distinct user

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,15 @@
 import jsonPlaceholder from "../components/jsonPlaceholder";
+import _ from 'lodash';
 
 const fetchPostsAndUsers = () => {
     return async function (dispatch, getState) {
         await dispatch(fetchPosts()); // Wait to fetch posts
         const posts = getState().posts; // Get all posts
-        let users = [];
         // get unique user id
-        for (let post of posts) {
-            if(users.indexOf(post.userId)===-1) {
-                users.push(post.userId);
-                dispatch(fetchAuthor(post.userId));
-            }
+        const users = _.uniq(posts.map(post => post.userId));
+        for (let user of users) {
+            dispatch(fetchAuthor(user));
         }
-
     }
 }
 
